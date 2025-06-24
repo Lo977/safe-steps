b9a801d7686a67d74184a8f4c45df04e8d74fb25
Buddha Lo <lo_977@yahoo.com>
2025-06-23 17:29:29 -0700

    add Logout function

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,7 +1,26 @@
-import React from "react";
-import { NavLink } from "react-router-dom";
+import React, { useContext } from "react";
+import { NavLink, useNavigate } from "react-router-dom";
+import UserContext from "./UserContext";
 
 function Navbar() {
+  const { setUser } = useContext(UserContext);
+  const navigate = useNavigate();
+
+  const handleLogout = () => {
+    fetch("/logout", {
+      method: "DELETE",
+      credentials: "include",
+    })
+      .then((res) => {
+        if (res.ok) {
+          setUser(null);
+          navigate("/login");
+        } else {
+          throw new Error("Logout failed");
+        }
+      })
+      .catch((err) => console.error(err));
+  };
   return (
     <nav className="navbar">
       <li>
@@ -9,6 +28,11 @@ function Navbar() {
           Home
         </NavLink>
       </li>
+      <div className="nav-actions">
+        <button onClick={handleLogout} className="logout-button">
+          Logout
+        </button>
+      </div>
     </nav>
   );
 }
