eb095216b51baeddb5ae66d034dd94f213bdfa9b
Buddha Lo <lo_977@yahoo.com>
2025-06-23 17:11:34 -0700

    complete Home.js

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,7 +1,19 @@
 import React from "react";
+import logo from "./SafeSteps.png";
 
 function Home() {
-  return <div>Home</div>;
+  return (
+    <div className="home-container">
+      <img src={logo} alt="" />
+      <h1 className="home-title">Welcome to SafeSteps</h1>
+      <p className="intro-text">
+        SafeSteps is a secure, trauma-informed web app designed to help
+        survivors of abuse, trafficking, or unsafe housing track their support
+        journey — including access to critical resources and support logs. This
+        platform is built to give you back clarity, control, and safety.
+      </p>
+    </div>
+  );
 }
 
 export default Home;
