106bd215ac3ae2f8c9ab9891d54d060eba658106
Buddha Lo <lo_977@yahoo.com>
2025-06-23 17:21:16 -0700

    create check_session

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,20 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
 
 function App() {
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    fetch("/check_session").then((res) => {
+      if (res.ok) {
+        res.json().then(setUser);
+      } else {
+        setUser(null);
+      }
+    });
+  }, []);
   return (
     <div>
       <Router>
